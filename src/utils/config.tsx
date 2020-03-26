6c88d74fd3b65d3f3d898b3808443c924fa77db6
Parthiva <patbhoyar18@gmail.com>
2020-03-27 01:09:30 +0530

    initial commit

---
+++ src/utils/config.tsx
@@ -0,0 +1,6 @@
+const API_KEY = "1XXCOF0APJ97RSNO"
+
+// https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=INFY.BSE&apikey=1XXCOF0APJ97RSNO
+// https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=INFY&apikey=1XXCOF0APJ97RSNO
+
+export {API_KEY}
